223e40ee4bf2de69e3f087cafc44b2afb40ff17f
Vladimir Fedin <vladimir.fedin@iu-study.org>
2024-11-17 22:00:10 +0100

    Add modal to append new city to the list

--- ui/pages/List/ListPage.jsx
+++ ui/pages/List/ListPage.jsx
@@ -1,5 +1,16 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {Card, CardBody, CardFooter, Image, Spinner} from '@nextui-org/react';
+import {
+    Card,
+    CardBody,
+    CardFooter,
+    Image,
+    Spinner,
+    Divider,
+    Button,
+    useDisclosure,
+} from '@nextui-org/react';
+
+import {NewCityModal} from './Modal/NewCityModal.jsx';
 
 function getCityMapImageUrl(city) {
     const params = new URLSearchParams({
@@ -13,6 +24,12 @@ function getCityMapImageUrl(city) {
 export function ListPage() {
     const [isLoading, setIsLoading] = useState(true);
     const [cityData, setCityData] = useState([]);
+
+    const {
+        isOpen: isModalOpen,
+        onOpen: onModalOpen,
+        onOpenChange: onModalOpenChange
+    } = useDisclosure();
 
     const fetchListPageData = useCallback(async () => {
         const response = await fetch('/api/list');
@@ -33,27 +50,48 @@ export function ListPage() {
     }
 
     return (
-        <div className="gap-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 md:gap-4">
-            {cityData.map((item, index) => (
-                <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
-                    <CardBody className="overflow-visible p-0">
-                        <Image
-                            shadow="sm"
-                            radius="lg"
-                            width="100%"
-                            alt={item.name}
-                            className="w-full object-cover h-[140px]"
-                            src={getCityMapImageUrl(item)}
-                        />
-                    </CardBody>
-                    <CardFooter className="text-small justify-between">
-                        <b>{item.name}</b>
-                        <p className="text-default-500">
-                            {Math.round(item.main.temp_min)}° — {Math.round(item.main.temp_max)}°
-                        </p>
-                    </CardFooter>
-                </Card>
-            ))}
+        <div className="flex flex-wrap w-full gap-4">
+            <div className="flex flex-wrap w-full justify-between pt-6">
+                <div className="flex items-center">
+                    <h2 className="text-2xl md:text-xl font-medium">Temperature in the cities</h2>
+                </div>
+                <Button
+                    className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+                    onPress={onModalOpen}
+                >
+                    Add
+                </Button>
+            </div>
+
+            <Divider />
+
+            <div className="w-full gap-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 md:gap-4">
+                {cityData.map((item, index) => (
+                    <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
+                        <CardBody className="overflow-visible p-0">
+                            <Image
+                                shadow="sm"
+                                radius="lg"
+                                width="100%"
+                                alt={item.name}
+                                className="w-full object-cover h-[140px]"
+                                src={getCityMapImageUrl(item)}
+                            />
+                        </CardBody>
+                        <CardFooter className="text-small justify-between">
+                            <b>{item.name}, {item.sys.country}</b>
+                            <p className="text-default-500">
+                                {Math.round(item.main.temp_min)}° — {Math.round(item.main.temp_max)}°
+                            </p>
+                        </CardFooter>
+                    </Card>
+                ))}
+            </div>
+
+            <NewCityModal
+                isOpen={isModalOpen}
+                onOpenChange={onModalOpenChange}
+            />
         </div>
     );
 }
---
+++ ui/pages/List/Modal/NewCityModal.jsx
@@ -0,0 +1,88 @@
+import React, {useCallback, useState} from 'react';
+import {debounce} from 'lodash';
+import {
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Button,
+    Input,
+    Spinner,
+    Chip,
+} from '@nextui-org/react';
+
+export function NewCityModal({isOpen, onOpenChange}) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [suggestions, setSuggestions] = useState([]);
+    const [selectedSuggestionId, setSelectedSuggestionId] = useState(null);
+
+    const searchForCity = useCallback(async (city) => {
+        setIsLoading(true);
+        setSuggestions([]);
+        setSelectedSuggestionId(null);
+
+        try {
+            const params = new URLSearchParams();
+            params.append('city', city);
+
+            const response = await fetch(`/api/suggest?${params.toString()}`);
+            const data = await response.json();
+
+            setSuggestions(data.list || []);
+        } catch (error) {
+            console.error(error);
+        }
+
+        setIsLoading(false);
+    }, []);
+
+    return (
+        <Modal isOpen={isOpen} onOpenChange={(value) => {
+            onOpenChange(value);
+            setSuggestions([]);
+            setSelectedSuggestionId(null);
+        }}>
+            <ModalContent>
+                {(onClose) => (
+                    <>
+                        <ModalHeader className="flex flex-col gap-1">Add new city</ModalHeader>
+                        <ModalBody>
+                            <p>Search for the city and selected it from the suggestions</p>
+                            <Input
+                                type="city"
+                                label="City"
+                                placeholder="Enter city"
+                                onValueChange={debounce(searchForCity, 1000)}
+                            />
+                            <div className="flex flex-wrap w-full gap-4">
+                                {isLoading ? (
+                                    <Spinner />
+                                ) : (
+                                    suggestions.map((item, index) => (
+                                        <Chip
+                                            size="md"
+                                            key={index}
+                                            color={selectedSuggestionId === index ? 'primary' : 'default'}
+                                            onClick={() => setSelectedSuggestionId(index)}
+                                        >
+                                            {item.name}, {item.sys.country}
+                                        </Chip>
+                                    ))
+                                )}
+                            </div>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="danger" variant="light" onPress={onClose}>
+                                Close
+                            </Button>
+                            <Button color="primary" onPress={onClose}>
+                                Save
+                            </Button>
+                        </ModalFooter>
+                    </>
+                )}
+            </ModalContent>
+        </Modal>
+    );
+}
